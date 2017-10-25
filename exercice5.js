9d6aeb4fe37b8fed7025af23ce198825b33d7efa
AndreVictor60 <andre.victor60@free.fr>
2017-10-25 14:43:57 +0200

    Vérification

--- exercice5.js
+++ exercice5.js
@@ -4,13 +4,12 @@ function resultat(){
   var number2 = document.getElementById('deuxieme_nombre').value;
   // Vérification
    if(isNaN(number1) == true || isNaN(number2) == true){
-      alert('Ceci n\est pas un chiffre');
+      alert('Ceci n\'est pas un chiffre');
    }
    else {
   //  renvoie un entier a partir d'une chaîne de caractères
-  number1 = parseInt(number1);
-  // transformer une chaîne de caractères en un nombre flottant
-  number2 = parseFloat(number2);
+  // number1 = parseInt(number1);
+  number1 = Math.trunc(premier_nombre);
   // Multiplication des nombres
   alert('Le résultat est '+ number1 * number2);
   }
